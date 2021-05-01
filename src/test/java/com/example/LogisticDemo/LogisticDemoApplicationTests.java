7e2b326790cad1b2f33a975760c60f97135e373a
EugeniySichev <sichev91@inbox.ru>
2021-05-01 16:25:09 +0300

    Initial commit

---
+++ src/test/java/com/example/LogisticDemo/LogisticDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogisticDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
